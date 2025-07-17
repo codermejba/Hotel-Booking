2bfd7bcd1f147a09e7a10982d50cf154e320ae6d
Mejba Hasan <mejbahassan135@gmail.com>
2025-07-17 17:46:55 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,35 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from './config/db.js'
-import authRoutes from './routes/authRoutes.js';
-import blogRoutes from './routes/blogRoutes.js'
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import blogRoutes from "./routes/blogRoutes.js";
 dotenv.config();
-
-
 
 const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://hotel-booking-beta-wine.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: [
+    "http://localhost:5173",
+    "https://hotel-booking-beta-wine.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 
 app.get("/", (req, res) => {
-    res.send("Hello from server");
-})
-app.use('/api/auth', authRoutes);
-app.use('/api/blogs', blogRoutes);
+  res.send("Hello from server");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/blogs", blogRoutes);
 
 connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-})
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
